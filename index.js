23c5710df2d246b28524babe04ee9fc6a145647d
Arthur Ilyashenko <ilyashenko.arthur@gmail.com>
2023-05-04 13:10:50 +0300

    Add changes for vercel deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routes = require("./routes/api");
+const routes = require("./api/routes/api");
 require("dotenv").config();
 
 const app = express();
